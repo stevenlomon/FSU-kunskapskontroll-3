d5b77beb5542f5ebd5ee75e4b9143c1fa5068cdb
StevenCesario <steven.lennartsson@gmail.com>
2026-02-11 10:47:12 +0000

    import fetchAll and fetchById!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import { fetchAll, fetchById } from "./api";
+
 const mainContainer = document.getElementById('container');
 
 // Prompt 6: Write an object called DataStore that should have an empty array called allTracks, a method getTracks that returns a JSON parsed object from localStorage under the key 'trackData' or an empty array; a method called setTracks that simply takes a tracks object and sets allTracks using `this`; and finally a getTracks method that returns allTracks using `this`.
