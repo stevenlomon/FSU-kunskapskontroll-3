e216479672f29032562a1f0ae7b758a3e1b56e6d
StevenCesario <steven.lennartsson@gmail.com>
2026-02-11 06:23:22 +0000

    prompt 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,2 @@
 // Prompt 6 to be added here
+// Prompt 6: Write an object called DataStore that should have an empty array called allTracks, a method getTracks that returns a JSON parsed object from localStorage under the key 'trackData' or an empty array; a method called setTracks that simply takes a tracks object and sets allTracks using `this`; and finally a getTracks method that returns allTracks using `this`.
