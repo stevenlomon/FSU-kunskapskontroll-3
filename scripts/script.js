69f8f54e0ed41a412175909cd8f009071f54cdfb
StevenCesario <steven.lennartsson@gmail.com>
2026-02-11 08:36:17 +0000

    init commit; mainContainer and ViewRenderer object defined

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const mainContainer = document.getElementById('container');
+
 // Prompt 6: Write an object called DataStore that should have an empty array called allTracks, a method getTracks that returns a JSON parsed object from localStorage under the key 'trackData' or an empty array; a method called setTracks that simply takes a tracks object and sets allTracks using `this`; and finally a getTracks method that returns allTracks using `this`.
 const DataStore = {
     // We store and "cache" the list here so that we don't have to fetch
@@ -21,3 +23,7 @@ const DataStore = {
         return this.allTracks;
     }
 };
+
+const ViewRenderer = {
+    // Prompt 9 to be added here
+}
