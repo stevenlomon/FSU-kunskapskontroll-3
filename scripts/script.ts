353a494686cc7ae62500e247da04c5cefeab4c3e
StevenCesario <steven.lennartsson@gmail.com>
2026-02-14 18:09:52 +0000

    fix: DataStore.saveAccessTokenToStorage should have token.access_token (string) as its input argument + tokenExpiration should be stored as a string so we need to wrap Date.now() + 3600 inside String()

--- scripts/script.ts
+++ scripts/script.ts
@@ -102,10 +102,10 @@ async function init() {
         // Prompt 18: Use getAccessTokenFromStorage to check to see if we have an Access Token in localStorage. If we don't, call generateAccessToken to generate one and save it to localStorage with saveAccessTokenToStorage
         if (!DataStore.getAccessTokenFromStorage()) {
             const token = await generateAccessToken();
-            DataStore.saveAccessTokenToStorage(token);
+            DataStore.saveAccessTokenToStorage(token.access_token);
 
             // Prompt 19: Also store an expiration timestamp (current time + 3600ms) in localStorage
-            localStorage.setItem('tokenExpiration', Date.now() + 3600);
+            localStorage.setItem('tokenExpiration', String(Date.now() + 3600));
         }
 
         // Fetch initial list and store it as cache in our DataStore
